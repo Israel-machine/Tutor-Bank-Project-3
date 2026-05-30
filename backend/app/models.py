d47e74c24958396d522794490ef2d1192c9ffa41
Israel-machine <israel.diaz1701@gmail.com>
2026-05-30 16:23:36 -0700

    Student Table Updated

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,26 +4,37 @@ from datetime import date
 
 db = SQLAlchemy()
 
-class User:
+class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column()
-    username = db.Column()
-    password_hash = db.Column()
-    #missing relation
 
-class Student:
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    students = db.relationship('Student', backref='tutor', lazy=True, cascade="all, delete-orphan")
+    #Need to set password
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')    
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Student(db.Model):
     __tablename__ = 'students'
-    id = db.Column()
-    student_first_name = db.Column()
-    student_last_name = db.Column()
-    school = db.Column()
-    school_grade = b.Column()
-    billing_address = db.Column()
-    contact_name = db.Column()
-    contact_phone = db.Column()
-    contact_email = db.Column()
-    user_id = db.Column()
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_first_name = db.Column(db.String(50), nullable=False)
+    student_last_name = db.Column(db.String(50), nullable=False)
+    school = db.Column(db.String(100))
+    school_grade = db.Column(db.String(20))
+    #use db.Text for varied address length
+    billing_address = db.Column(db.Text)
+    contact_name = db.Column(db.String(50), nullable=False)
+    contact_phone = db.Column(db.String(20))
+    contact_email = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     #missing relation
+    sessions = db.relationship('Session', backref='student', lazy=True, cascade="all, delete-orphan")
 
 
 class Session:
